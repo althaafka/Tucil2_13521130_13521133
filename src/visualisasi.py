e8539a2c54033261ad3c8487dd64db47a1a284d5
CettaReswara <88731709+CettaReswara@users.noreply.github.com>
2023-03-01 12:00:33 +0700

    Update Visualisasi, Add Laporan

--- src/visualisasi.py
+++ src/visualisasi.py
@@ -5,10 +5,13 @@ import plotly
 import plotly.express as px
 
 def makegraph(points, closestPair) :
+    # Mengubah list point dan closestPair menjadi array
     npoints = np.array(points)
     nclosest = np.array(closestPair)
 
+    # Untuk kasus 3D
     if len(nclosest[0]) == 3 :
+        # Mengubah array menjadi dataframe
         df = pd.DataFrame({'x':npoints[:,0], 
                            'y':npoints[:,1],
                            'z':npoints[:,2]})
@@ -17,26 +20,34 @@ def makegraph(points, closestPair) :
                               'y':nclosest[:,1],
                               'z':nclosest[:,2]})
     
+        # Memberi attribut Closest TRUE pada point yang menjadi closestPair
         d = pd.merge(df, newdf, on=['x','y','z'], how='left', indicator='Closest')
         d['Closest'] = np.where(d.Closest == 'both', True, False)
 
+        # Visualisasi data
         fig3d = px.scatter_3d(d, x='x', y='y', z='z', color='Closest')
         fig3d.show()
 
+    # Untuk kasus 2D
     elif len(nclosest[0]) == 2 :
+        # Mengubah array menjadi dataframe
         df = pd.DataFrame({'x':npoints[:,0], 
                            'y':npoints[:,1]})
     
         newdf = pd.DataFrame({'x':nclosest[:,0],
                               'y':nclosest[:,1]})
     
+        # Memberi attribut Closest TRUE pada point yang menjadi closestPair
         d = pd.merge(df, newdf, on=['x','y'], how='left', indicator='Closest')
         d['Closest'] = np.where(d.Closest == 'both', True, False)
 
+         # Visualisasi data
         fig2d = px.scatter(d, x='x', y='y', color='Closest')
         fig2d.show()
 
+    # Untuk kasus N-Dimensi
     else :
+        # Mengubah array menjadi dataframe
         trydf = pd.DataFrame()
         for i in range (len(nclosest[0])) :
             trydf[i] = npoints[:,i]
@@ -45,11 +56,13 @@ def makegraph(points, closestPair) :
         for i in range (len(nclosest[0])) :
             trynewdf[i] = nclosest[:,i]
 
+        # Memberi attribut Closest TRUE pada point yang menjadi closestPair
         tryattribut = list(range(len(nclosest[0])))
 
         tryd = pd.merge(trydf, trynewdf, on=tryattribut, how='left', indicator='Closest')
         tryd['Closest'] = np.where(tryd.Closest == 'both', True, False)
 
+         # Visualisasi data
         figmatrix = px.scatter_matrix(tryd, color='Closest')
         figmatrix.show()
 
